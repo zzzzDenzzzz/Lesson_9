8d7acd772df7576b6c4cbbd23e563bc404accb43
12den24@gmail.com <12den24@gmail.com>
2022-09-28 20:39:58 +0300

    add

--- Lesson_9/Program.cs
+++ Lesson_9/Program.cs
@@ -7,7 +7,7 @@ namespace Lesson_9
         static void Main(string[] args)
         {
             var pet = new Pet("Face", 60000);
-            var pet1 = new Pet("Face", 80000);
+            var pet1 = new Pet("Ecaf", 80000);
 
             var thread = new Thread(pet.Game);
             var thread1 = new Thread(pet1.Game);
